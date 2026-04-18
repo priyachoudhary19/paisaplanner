6a1a69c8233e097b346d861a0061232361b81fee
Priya Choudhary <priyachoudhary19@gmail.com>
2026-04-18 10:18:51 +0530

    updated charts and graph section and time

--- project/project/settings.py
+++ project/project/settings.py
@@ -114,9 +114,8 @@ AUTH_PASSWORD_VALIDATORS = []
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Asia/Kolkata' 
 
-TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
